45fda9372a62978489db4ea91889858d80d669a4
Chelsie1234 <chelsie.louis@efrei.net>
2023-10-29 16:48:03 +0100

    Database with the triggers

--- sound_spot_db.sql
+++ sound_spot_db.sql
@@ -1,8 +1,10 @@
--- Active: 1698513531912@@127.0.0.1@3306
-CREATE DATABASE sound_spot
-    DEFAULT CHARACTER SET = 'utf8mb4';
+-- Active: 1698594207995@@127.0.0.1@3306@sound_spot
+-- CREATE DATABASE sound_spot
+--     DEFAULT CHARACTER SET = 'utf8mb4';
 
-USE sound_spot;
+-- USE sound_spot;
+
+-- SHOW TABLES;
 
 CREATE TABLE IF NOT EXISTS MAIN_ARTIST
 (
@@ -28,4 +30,33 @@ CREATE TABLE IF NOT EXISTS TRACK
     FOREIGN KEY (main_artist_id) REFERENCES MAIN_ARTIST(main_artist_id)
 );
 
--- CREATE OR REPLACE TRIGGER
+DELIMITER //
+-- Trigger to avoid duplicate tracks
+CREATE TRIGGER check_existing_track
+BEFORE INSERT ON TRACK
+FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT 1 FROM TRACK WHERE track_id = NEW.track_id) OR
+       EXISTS (SELECT 1 FROM TRACK WHERE main_artist_id = NEW.main_artist_id) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'This track or main artist already exists in the database!';
+    END IF;
+END;
+//
+
+-- Trigger to avoid duplicate artists
+CREATE TRIGGER check_existing_main_artist
+BEFORE INSERT ON MAIN_ARTIST
+FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT 1 FROM MAIN_ARTIST WHERE main_artist_id = NEW.main_artist_id) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'This artist already exists in the database!';
+    END IF;
+END;
+//
+DELIMITER ;
+
+SHOW TRIGGERS;
+
+-- SELECT * FROM track;
